dd120f72093bba5e2c1d48044ac2327b13bdac83
Henrique Bovo <henrique.bovo@usp.br>
2023-07-13 22:31:15 -0300

    feat: implement client/server socket foundation

--- include/server.h
+++ include/server.h
@@ -1,3 +1,17 @@
 #pragma once
+#include <sys/socket.h>
 
-void runServer();
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 3000
+#define MAX_CONNECTED_CLIENTS 20
+
+// Gets server ip on sockaddr_in format from defined string SERVER_IP (server.h).
+struct sockaddr_in get_server_sockaddr();
+
+/* Starts server, sets it to be ready to start new connections (listen) and returns
+int: socket file description (socket id). */
+int create_server();
+
+void set_server_to_listening_mode(int server_socket);
+
+void shutdown_server(int server_socket);
--- src/client.c
+++ src/client.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
+#include "server.h"
 #include "client.h"
 
-void runClient() {
-  printf("Client running.\n");
+void run_client() {
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    printf("Error starting client socket. Shutting down.\n");
+    return;
+  }
+
+  const struct sockaddr_in SERVER_ADDRESS = get_server_sockaddr();
+
+  if (connect(sockfd, (struct sockaddr *)&SERVER_ADDRESS, sizeof(SERVER_ADDRESS)) == -1) {
+    printf("Error connecting to the server. Shutting down.\n");
+    shutdown_client(sockfd);
+    return;
+  }
+
+  printf("Client succesfully connected and running.\n");
 }
+
+void shutdown_client(int client_socket) { close(client_socket); }
